0df8260c7538259102a94dde07fd002c98418a43
Jodelfi <jodelfimarimba56@gmail.com>
2024-11-18 15:54:15 +0100

    style: lazy loading e outros estilos

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -1,25 +1,29 @@
-function createSlider(contentArray, sectionId, prevButtonId, nextButtonId) {
+function createSlider(contentArray, sectionId, prevButtonId, nextButtonId, backgroundID) {
     let currentIndex = 0;
     let intervalId;
     let startX = 0;
     let endX = 0;
-
+    const backimage = document.getElementById(backgroundID)
     const hlsection = document.getElementById(sectionId);
     const heroContentContainer = hlsection.querySelector('.section-content');
 
     function updateHero() {
-        hlsection.style.backgroundImage = contentArray[currentIndex].background;
+        backimage.setAttribute('data-src', contentArray[currentIndex].background);
         heroContentContainer.innerHTML = contentArray[currentIndex].content;
     }
+
+
 
     function nextHero() {
         currentIndex = (currentIndex + 1) % contentArray.length;
         updateHero();
+        LazyLoading();
     }
 
     function prevHero() {
         currentIndex = (currentIndex - 1 + contentArray.length) % contentArray.length;
         updateHero();
+        LazyLoading();
     }
 
     // function startAutoRotate() {
@@ -34,11 +38,11 @@ function createSlider(contentArray, sectionId, prevButtonId, nextButtonId) {
     document.getElementById(prevButtonId).addEventListener('click', prevHero);
 
     // Touch events for swiping
-    hlsection.addEventListener('touchstart', (event) => {
+    backimage.addEventListener('touchstart', (event) => {
         startX = event.touches[0].clientX;
     });
 
-    hlsection.addEventListener('touchend', (event) => {
+    backimage.addEventListener('touchend', (event) => {
         endX = event.changedTouches[0].clientX;
         handleSwipe();
     });
@@ -60,21 +64,21 @@ function createSlider(contentArray, sectionId, prevButtonId, nextButtonId) {
 
 const highContent = [
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00014.webp")',
+        background: './content/photo/image00014.webp',
         content: `
             <h2>Pomigliano W <text>VS</text> AC MILAN</h2>
             <h2>0 <text>-</text> 4</h2>
             <p>Chanté-Mary, scores 2 goals in AC Milan's victory against Pomigliano W, ending the game with a difference of 4 - 0.</p>`
     },
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00013.webp")',
+        background: './content/photo/image00013.webp',
         content: `
             <h2>JUSTIMAGINE <text>VS</text> AC MILAN</h2>
             <h2>0 <text>-</text> 8</h2>
             <p>Chanté-Mary, scores 2 goals in AC Milan's victory against JUSTIMAGINE, ending the game with a difference of 4 - 0.</p>`
     },
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00033.webp")',
+        background: './content/photo/image00033.webp',
         content: `
             <h2>This is <text>JUST</text> An Example</h2>
             <h2>4 <text>-</text> 4</h2>
@@ -91,19 +95,19 @@ const highContent = [
 
 const nationalContent = [
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00028.webp")',
+        background: './content/photo/image00028.webp',
         content: `
         
            `
     },
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00029.webp")',
+        background: './content/photo/image00029.webp',
         content: `
         
            `
     },
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00030.webp")',
+        background: './content/photo/image00030.webp',
         content: `
         
            `
@@ -113,7 +117,7 @@ const nationalContent = [
 
 const brandsContent = [
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00001.webp")',
+        background: './content/photo/image00001.webp',
         content: `
             <p><text>PUMA</text>  OFF-WHITE  MATCHDAY</p>
            `
@@ -125,21 +129,22 @@ const brandsContent = [
     //        `
     // },
     {
-        background: 'url("../content/photo/OFF WHITE/WhatsApp Image 2024-07-14 at 03.03.20 (3).webp")',
+        background: './content/photo/OFF WHITE/WhatsApp Image 2024-07-14 at 03.03.20 (3).png',
         content: `
             <p>PUMA  <text>OFF-WHITE</text>  MATCHDAY</p>
            `
     },
     {
-        background: 'linear-gradient(180deg, #ffffff00 0%, #6f717106 63%, #22242453 79%), url("../content/photo/image00008.webp")',
+        background: './content/photo/image00008.webp',
         content: `
             <p>PUMA  OFF-WHITE  <text>MATCHDAY</text></p>
            `
     }
 ]
-createSlider(highContent , 'highlights', 'prev-button1', 'next-button1');
-createSlider(nationalContent , 'national', 'prev-button2', 'next-button2');
-createSlider(brandsContent , 'brands', 'prev-button3', 'next-button3');
+
+    createSlider(highContent , 'highlights', 'prev-button1', 'next-button1', 'highlight-bg');
+    createSlider(nationalContent , 'national', 'prev-button2', 'next-button2', 'national-bg');
+    createSlider(brandsContent , 'brands', 'prev-button3', 'next-button3', 'brand-bg');
 
 
 function changeColorMode(direction) {
